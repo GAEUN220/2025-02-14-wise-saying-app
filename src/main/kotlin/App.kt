94cf01abb311c969e896ea5d57a7984f03bd6e03
gaeun220 <gaeun2701@gmail.com>
2025-02-14 15:13:56 +0900

    level 6

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -12,10 +12,10 @@ class App {
             print("명령) ")
             val input = readlnOrNull()!!.trim()
 
-            when (input) {
-                "종료" -> {
+            when {
+                input == "종료" -> {
                     break
-                } "등록" -> {
+                } input == "등록" -> {
                     print("명언 : ")
                     val content = readlnOrNull()!!.trim()
                     print("작가 : ")
@@ -25,7 +25,7 @@ class App {
                     wiseSayings.add(WiseSaying(id, content, author))
 
                     println("${id}번 명언이 등록되었습니다.")
-                } "목록" -> {
+                } input == "목록" -> {
                     if (wiseSayings.isEmpty()) {
                         println("등록된 명언이 없습니다.")
                     } else {
@@ -36,7 +36,24 @@ class App {
                             println("${it.id} / ${it.author} / ${it.content}")
                         }
                     }
+                } input.startsWith("삭제?id=") -> {
+                    val idStr = input.substringAfter("삭제?id=")
+                    val id = idStr.toIntOrNull()
+
+                    if (id == null ) {
+                        println("잘못된 id 형식입니다.")
+                        continue
+                    }
+
+                    val removed = wiseSayings.removeIf{ it.id == id }
+
+                    if (removed) {
+                        println("${id}번 명언이 삭제되었습니다.")
+                    } else {
+                        println("해당 명언은 존재하지 않습니다.")
+                    }
                 }
+                else -> println("알 수 없는 명령어입니다.")
             }
         }
     }
