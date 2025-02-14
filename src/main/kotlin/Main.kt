4061889a30052ecd84c05d1e6064cce4fa0a95c6
gaeun220 <gaeun2701@gmail.com>
2025-02-14 14:45:32 +0900

    level 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 package com.ll
 
+data class WiseSaying(val id: Int, val quote: String, val author: String)
+
 fun main() {
     var lastId = 0
+    val wiseSayings = mutableListOf<WiseSaying>()
 
     println("== 명언 앱 ==")
 
@@ -9,18 +12,28 @@ fun main() {
         print("명령) ")
         val input = readLine()
 
-        if (input == "종료") {
-            println("종료")
-            break
-        } else if ( input == "등록" ) {
-            print("명언 : ")
-            val wiseSaying = readLine()
-            print("작가 : ")
-            val author = readLine()
+        when (input) {
+            "종료" -> {
+                break
+            } "등록" -> {
+                print("명언 : ")
+                val quote = readLine()!!
+                print("작가 : ")
+                val author = readLine()!!
 
-            lastId ++
+                lastId ++
+                val wiseSaying = WiseSaying(lastId, quote, author)
+                wiseSayings.add(wiseSaying)
 
-            println("%d번 명언이 등록되었습니다.".format(lastId))
+                println("${lastId}번 명언이 등록되었습니다.")
+            } "목록" -> {
+                println("번호 / 작가 / 명언")
+                println("----------------------")
+
+                wiseSayings.reversed().forEach {
+                    println("${it.id} / ${it.author} / ${it.quote}")
+                }
+            }
         }
     }
 }
